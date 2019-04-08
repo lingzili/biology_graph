4c52b057cd17a016240afec6774c80c8ce8dffe5
lingzili <lingzili@bmb.sdu.dk>
2019-04-08 16:15:05 +0200

    t.test

--- R/pValue.R
+++ R/pValue.R
@@ -15,7 +15,7 @@ for (i in unique(BW_long$Sex)) {
   GcgcreTRAP <- as.numeric(as.character(unlist(df$GcgcreTRAP)))
   TRAP <- as.numeric(as.character(unlist(df$TRAP)))
 
-  p <- wilcox.test(GcgcreTRAP, TRAP, correct = FALSE)
+  p <- t.test(GcgcreTRAP, TRAP)
 
   p_BW <- append(p_BW, i)
   
@@ -26,3 +26,31 @@ for (i in unique(BW_long$Sex)) {
 p_BW
 
 save(p_BW, file = "data/p_BW_week0.RData")
+
+# Blood glucose -----------------------------------------------------------
+# Is there any significant difference between GcgcreTRAP and TRAP?
+# Week 0
+p_Glc <- list()
+
+for (i in unique(Glc_long$Sex)) {
+  df <- Glc_long %>%
+    filter(Sex == i, Week == "Week 0") %>%
+    spread(Genotype, Glycemia)
+  
+  print(df)
+  
+  GcgcreTRAP <- as.numeric(as.character(unlist(df$GcgcreTRAP)))
+  TRAP <- as.numeric(as.character(unlist(df$TRAP)))
+  
+  p <- t.test(GcgcreTRAP, TRAP)
+  
+  p_Glc <- append(p_Glc, i)
+  
+  p_Glc <- append(p_Glc, p)
+  
+}
+
+p_Glc
+
+save(p_Glc, file = "data/p_Glc_week0.RData")
+
