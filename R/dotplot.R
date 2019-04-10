8b07f3c6b61c8f8434fb9c7409c84635691c5843
lingzili <lingzili@bmb.sdu.dk>
2019-04-10 23:16:46 +0200

    p values

--- R/dotplot.R
+++ R/dotplot.R
@@ -1,7 +1,6 @@
 # Boxplot with dotplot
 library(tidyverse)
 library(readxl)
-library(Hmisc)
 
 # Body weight (g) ---------------------------------------------------------
 # Load data set
@@ -44,6 +43,37 @@ week0_p2
 
 ggsave(here::here("graph/BW_week0.png"), week0_p2) # Save 7.36 x 4.55 in image
 
+# Percentage of weight gain -----------------------------------------------
+# Calculate percentage of weight gain
+BW$Percent_Week1 <- (BW$`Week 1`/BW$`Week 0` - 1) * 100
+
+# Rank the sex
+BW$Sex <- factor(BW$Sex, levels = c("Male", "Female"))
+
+week1_p1 <- BW %>%
+  ggplot(aes(x = Diet, y = Percent_Week1, fill = Diet)) +
+  geom_boxplot(position = position_dodge(0.8)) +
+  geom_jitter(width = .05, size = 3) +
+  facet_grid(cols = vars(Sex))
+
+week1_p2 <- week1_p1 +
+  ylim(-5, 20) +
+  labs(title = "1 week of HFD: Weight gain normalized to T0", x = NULL, y = "Percentage of weight gain (%)") +
+  guides(fill = "none") + # Remove legends
+  theme(
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(color = "black", size = 14, face = "bold"),
+    axis.text.y = element_text(color = "black", size = 14, face = "bold"),
+    axis.title.y = element_text(color = "black", size = 14, face = "bold"),
+    strip.text.x = element_text(color = "black", size = 14),
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank()
+  )
+
+week1_p2
+
+ggsave(here::here("graph/Weight_Gain_Week1.png"), week1_p2) # Saving 7.36 x 4.43 in image
+
 # Fed glycemia (mM) -------------------------------------------------------
 # Load data set
 Glc <- read_excel("data/Gcg_Fed_Glycemia.xlsx", col_types = c("text", "text", "text", "text", "numeric", "numeric", "numeric", "numeric", "numeric"))
@@ -56,10 +86,10 @@ Glc_long <- Glc %>%
 
 View(Glc_long)
 
-# Plot week 0 data based on sex and genotype
 # Rank the sex
 Glc_long$Sex <- factor(Glc_long$Sex, levels = c("Male", "Female"))
 
+# Plot week 0 data based on sex and genotype
 week0_p1 <- Glc_long %>%
   filter(Week == "Week 0") %>%
   ggplot(aes(x = Genotype, y = Glycemia, fill = Genotype)) +
@@ -83,4 +113,108 @@ week0_p2 <- week0_p1 +
 
 week0_p2
 
-ggsave(here::here("graph/Fed_Glc_week0.png"), week0_p2) #Save 7.36 x 4.55 in image
+ggsave(here::here("graph/Fed_Glc_week0.png"), week0_p2) # Save 7.36 x 4.55 in image
+
+# Plot week 1 data based on sex and diet
+week1_p1 <- Glc_long %>%
+  filter(Week == "Week 1") %>%
+  ggplot(aes(x = Diet, y = Glycemia, fill = Diet)) +
+  geom_boxplot(position = position_dodge(0.8)) +
+  geom_jitter(width = .05, size = 3) +
+  facet_grid(cols = vars(Sex))
+
+week1_p2 <- week1_p1 +
+  ylim(0, 15) + # Set y axis
+  labs(title = "1 week of HFD: Glycemia", x = NULL, y = "Fed blood glucose (mM)") +
+  guides(fill = "none") + # Remove legends
+  theme(
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(color = "black", size = 14, face = "bold"),
+    axis.text.y = element_text(color = "black", size = 14, face = "bold"),
+    axis.title.y = element_text(color = "black", size = 14, face = "bold"),
+    strip.text.x = element_text(color = "black", size = 14),
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank()
+  )
+
+week1_p2
+
+ggsave(here::here("graph/Fed_Glc_week1.png"), week1_p2) # Save 7.36 x 4.43 in image
+
+# Body composition --------------------------------------------------------
+# Load data set
+Mass <- read_excel("data/Gcg_DEXA.xlsx", col_types = c(
+  "text", "text", "text", "text", "text",
+  "numeric", "numeric", "numeric"
+))
+View(Mass)
+
+# Calculate percentage of fat and lean mass
+Mass$Lean_percent <- Mass$Lean_gram / Mass$Total_gram * 100
+Mass$Fat_percent <- Mass$Fat_gram / Mass$Total_gram * 100
+
+# Change to long form
+Mass_long <- Mass %>%
+  select(-c(Lean_gram, Fat_gram, Total_gram)) %>%
+  gather(Composition, Percent, 6:7) %>%
+  arrange(ID)
+
+View(Mass_long)
+
+# Remove "_percent" from data labels
+Mass_long$Composition <- gsub("_percent", "", Mass_long$Composition)
+
+# Rank the sex
+Mass_long$Sex <- factor(Mass_long$Sex, levels = c("Male", "Female"))
+
+# Plot fat mass of week 1
+week1_p1 <- Mass_long %>%
+  filter(Composition == "Fat") %>%
+  ggplot(aes(x = Diet, y = Percent, fill = Diet)) +
+  geom_boxplot(position = position_dodge(0.8)) +
+  geom_jitter(width = .05, size = 3) +
+  facet_grid(cols = vars(Sex))
+
+week1_p2 <- week1_p1 +
+  ylim(5, 25) + # Set y axis
+  labs(title = "1 week of HFD: Fat mass", x = NULL, y = "Percentage of fat mass (%)") +
+  guides(fill = "none") + # Remove legends
+  theme(
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(color = "black", size = 14, face = "bold"),
+    axis.text.y = element_text(color = "black", size = 14, face = "bold"),
+    axis.title.y = element_text(color = "black", size = 14, face = "bold"),
+    strip.text.x = element_text(color = "black", size = 14),
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank()
+  )
+
+week1_p2
+
+ggsave(here::here("graph/Fat_mass_week1.png"), week1_p2) # Save 7.36 x 4.43 in image
+
+# Plot lean mass of week 1
+week1_p1 <- Mass_long %>%
+  filter(Composition == "Lean") %>%
+  ggplot(aes(x = Diet, y = Percent, fill = Diet)) +
+  geom_boxplot(position = position_dodge(0.8)) +
+  geom_jitter(width = .05, size = 3) +
+  facet_grid(cols = vars(Sex))
+
+week1_p2 <- week1_p1 +
+  ylim(75, 100) + # Set y axis
+  labs(title = "1 week of HFD: Lean mass", x = NULL, y = "Percentage of lean mass (%)") +
+  guides(fill = "none") + # Remove legends
+  theme(
+    axis.line = element_line(colour = "black"),
+    axis.text.x = element_text(color = "black", size = 14, face = "bold"),
+    axis.text.y = element_text(color = "black", size = 14, face = "bold"),
+    axis.title.y = element_text(color = "black", size = 14, face = "bold"),
+    strip.text.x = element_text(color = "black", size = 14),
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank()
+  )
+
+week1_p2
+
+ggsave(here::here("graph/Lean_mass_week1.png"), week1_p2) # Save 7.36 x 4.43 in image
